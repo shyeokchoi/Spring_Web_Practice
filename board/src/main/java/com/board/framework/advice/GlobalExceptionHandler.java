ca63c60a4e00c85f6eee04f958de901f88e0de12
shyeokchoi <shyeokchoi@gmail.com>
2023-09-08 16:16:02 +0900

    fix: 존재하지 않는 id를 입력하거나 틀린 비밀번호를 입력했을 때 internal error로 취급되는 문제 고치기

--- board/src/main/java/com/board/framework/advice/GlobalExceptionHandler.java
+++ board/src/main/java/com/board/framework/advice/GlobalExceptionHandler.java
@@ -3,14 +3,13 @@ package com.board.framework.advice;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.security.sasl.AuthenticationException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.board.exception.AuthenticationException;
 import com.board.exception.ConflictException;
 
 @RestControllerAdvice
@@ -31,7 +30,7 @@ public class GlobalExceptionHandler {
      * 이미 존재하는 회원 정보로 가입하려고 하면 발생하는 예외
      * 
      * @param e
-     * @return status code 409, 에러에 대한 메시지를 담고 있는 ResponseEntity
+     * @return status code 409
      */
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<Map<String, String>> handleConflictException(ConflictException e) {
@@ -42,13 +41,19 @@ public class GlobalExceptionHandler {
      * 잘못된 형식을 입력했을 때 발생하는 예외
      * 
      * @param e
-     * @return status code 400, 에러에 대한 메시지를 담고 있는 ResponseEntity
+     * @return status code 400
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(buildErrBody(e.getMessage()));
     }
 
+    /**
+     * 아이디와 비밀번호를 틀렸거나 존재하지 않는 아이디로 회원가입을 시도할 때 발생하는 예외
+     * 
+     * @param e
+     * @return status code 401
+     */
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Map<String, String>> handleAuthenticationException(AuthenticationException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(buildErrBody(e.getMessage()));
