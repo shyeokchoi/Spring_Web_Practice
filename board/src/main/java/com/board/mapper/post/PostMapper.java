fb598ce6bf868197623c15f8e862584e579b83b3
shyeokchoi <shyeokchoi@gmail.com>
2023-09-14 10:40:30 +0900

    comment: PostMapper 주석 추가

--- board/src/main/java/com/board/mapper/post/PostMapper.java
+++ board/src/main/java/com/board/mapper/post/PostMapper.java
@@ -14,18 +14,58 @@ import com.board.dto.post.UpdatePostDTO;
 
 @Mapper
 public interface PostMapper {
+    /**
+     * 이미 삭제된 게시글인지 확인
+     * 
+     * @param postNo
+     * @return
+     */
     public Boolean isAlreadyDeleted(Integer postNo);
 
+    /**
+     * 게시글 등록
+     * 
+     * @param insPosDTO
+     */
     public void insPost(InsPostDTO insPosDTO);
 
+    /**
+     * 게시글 상세정보 불러오기
+     * 
+     * @param postNo
+     * @return
+     */
     public SelectPostDetailDTO selectPost(Integer postNo);
 
+    /**
+     * 게시글 수정
+     * 
+     * @param updatePostDTO
+     */
     public void updatePost(UpdatePostDTO updatePostDTO);
 
+    /**
+     * 게시글 no로부터 해당 게시글 작성자의 no 불러오기
+     * 
+     * @param postNo
+     * @return
+     */
     public Integer retvAuthorNo(Integer postNo);
 
+    /**
+     * 게시글 삭제
+     * 
+     * @param postNo
+     */
     public void deletePost(Integer postNo);
 
+    /**
+     * 게시글 리스트 (+ 검색)
+     * 
+     * @param pagingDTO
+     * @param searchDTO
+     * @return
+     */
     public List<SelectPostListDTO> selectPostList(
             @Param("pagingDTO") PagingDTO pagingDTO,
             @Param("searchDTO") SearchDTO searchDTO);
